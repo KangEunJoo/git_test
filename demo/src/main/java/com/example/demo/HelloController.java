24c07bdb21d704fc559b508b6105713e7117a20c
KangEunJoo <kej8640@naver.com>
2022-10-05 21:23:02 +0900

    [update] Hello World 추가

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -9,6 +9,7 @@ import java.util.Date;
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello, the time at the server is now " + new Date() + "\n";
+        return "Hello, the time at the server is now " + new Date() + "\n"
+        		+"Hello World!!";
     }
 }
